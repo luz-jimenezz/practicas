2fbebddb1f3dbc772c392503d02e94cfe87273e4
luz-jimenez <jluz4224@gmail.com>
2023-07-07 11:20:42 -0300

    en el header.html creamos el menu, y agregamos los links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemsComponent } from './components/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -10,6 +10,21 @@ export class ItemsComponent implements OnInit {
     items: Item []=[] ;
     constructor() { }
     ngOnInit(): void {
-
+        this.items = [
+            {
+                id: 0,
+                title: 'practicas',
+                price: 10.5,
+                quantity: 4,
+                completed: false
+            },
+            {
+                id: 1,
+                title: 'practicas_profesionalisantes',
+                price: 3.5,
+                quantity: 8,
+                completed: true
+            }
+        ];
     }
 }
